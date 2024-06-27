d17ee931a342532f34e0bea3a8225ccb6ca4fd55
benson <mwangibenson923@gmail.com>
2024-06-27 09:47:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+const practice = document.getElementById("practice")
+const example =document.getElementsByClassName("example")
+const button = document.querySelector("button")
+ console.log(button.textContent);
+ const p = document.getElementsByTagName("p")
+ console.log(p);
